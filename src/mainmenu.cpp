d7c12c4bb5331159f120caf3640e9d3085413754
Marc1995 <marc.gaspar.martinez@gmail.com>
2018-02-08 10:13:14 +0100

    setFillColor->setColor i el makefile ara te colisions

--- src/mainmenu.cpp
+++ src/mainmenu.cpp
@@ -1,93 +1,93 @@
 #include "mainmenu.hpp"
 
-MainMenu::MainMenu(Game& game) : Scene(game) {}
-MainMenu::~MainMenu() {}
+	MainMenu::MainMenu(Game& game) : Scene(game) {}
+	MainMenu::~MainMenu() {}
 
-void MainMenu::onInit() 
-{
-	Scene::onInit();
-	font.loadFromFile("res/fonts/font.ttf");
-	textureCursor.loadFromFile("res/img/cursor.png");
+	void MainMenu::onInit() 
+	{
+		Scene::onInit();
+		font.loadFromFile("res/fonts/font.ttf");
+		textureCursor.loadFromFile("res/img/cursor.png");
 
-  cursorState = MainMenu::PLAY;
-}
+	  cursorState = MainMenu::PLAY;
+	}
 
-void MainMenu::onResume() 
-{
-	Scene::onResume();
-}
+	void MainMenu::onResume() 
+	{
+		Scene::onResume();
+	}
 
-void MainMenu::onPause()  
-{
-	Scene::onPause();
-}
+	void MainMenu::onPause()  
+	{
+		Scene::onPause();
+	}
 
-void MainMenu::onEnd() 
-{
-	Scene::onEnd();
-}
+	void MainMenu::onEnd() 
+	{
+		Scene::onEnd();
+	}
 
-void MainMenu::update(const sf::Time& deltatime) 
-{
+	void MainMenu::update(const sf::Time& deltatime) 
+	{
 
-}
+	}
 
-void MainMenu::draw(sf::RenderWindow& window) const 
-{
-	drawTexts(window);
-	drawCursor(window);
-}
+	void MainMenu::draw(sf::RenderWindow& window) const 
+	{
+		drawTexts(window);
+		drawCursor(window);
+	}
 
-void MainMenu::event(const sf::Event& event) 
-{
-	if (event.type == sf::Event::KeyPressed) 
-  {
-    switch (event.key.code) 
-    {
-      case sf::Keyboard::Up:
-        if (cursorState == MainMenu::PLAY)
-          cursorState = MainMenu::EXIT;
-        else if (cursorState == MainMenu::ABOUT)
-          cursorState = MainMenu::PLAY;
-        else if (cursorState == MainMenu::EXIT)
-          cursorState = MainMenu::ABOUT;
-        break;
+	void MainMenu::event(const sf::Event& event) 
+	{
+		if (event.type == sf::Event::KeyPressed) 
+	  {
+	    switch (event.key.code) 
+	    {
+	      case sf::Keyboard::Up:
+		if (cursorState == MainMenu::PLAY)
+		  cursorState = MainMenu::EXIT;
+		else if (cursorState == MainMenu::ABOUT)
+		  cursorState = MainMenu::PLAY;
+		else if (cursorState == MainMenu::EXIT)
+		  cursorState = MainMenu::ABOUT;
+		break;
 
-      case sf::Keyboard::Down:
-        if (cursorState == MainMenu::PLAY)
-          cursorState = MainMenu::ABOUT;
-        else if (cursorState == MainMenu::ABOUT)
-          cursorState = MainMenu::EXIT;
-        else if (cursorState == MainMenu::EXIT)
-          cursorState = MainMenu::PLAY;
-        break;
+	      case sf::Keyboard::Down:
+		if (cursorState == MainMenu::PLAY)
+		  cursorState = MainMenu::ABOUT;
+		else if (cursorState == MainMenu::ABOUT)
+		  cursorState = MainMenu::EXIT;
+		else if (cursorState == MainMenu::EXIT)
+		  cursorState = MainMenu::PLAY;
+		break;
 
-      case sf::Keyboard::Return:
-      	if (cursorState == MainMenu::PLAY) 
-        {
-      		game.setBackgroundScene(Scene::create(game, Scene::TEST_1));
-      		pause();
-      	} else if (cursorState == MainMenu::ABOUT) 
-        {
-      		game.setBackgroundScene(Scene::create(game, Scene::TEST_2));
-      		pause();
-      	} else if (cursorState == MainMenu::EXIT) 
-        {
-          end();
-      	}
-        break;
+	      case sf::Keyboard::Return:
+		if (cursorState == MainMenu::PLAY) 
+		{
+			game.setBackgroundScene(Scene::create(game, Scene::TEST_1));
+			pause();
+		} else if (cursorState == MainMenu::ABOUT) 
+		{
+			game.setBackgroundScene(Scene::create(game, Scene::TEST_2));
+			pause();
+		} else if (cursorState == MainMenu::EXIT) 
+		{
+		  end();
+		}
+		break;
 
-      default:
-        break;
-    }
-  }
-}
+	      default:
+		break;
+	    }
+	  }
+	}
 
-void MainMenu::drawTexts(sf::RenderWindow& window) const 
-{
-  sf::Text text;
-  text.setFont(font);
-  text.setFillColor(sf::Color::White);
+	void MainMenu::drawTexts(sf::RenderWindow& window) const 
+	{
+	  sf::Text text;
+	  text.setFont(font);
+	  text.setColor(sf::Color::White);
   text.setScale(2.f, 2.f);
 
   // Draw title
